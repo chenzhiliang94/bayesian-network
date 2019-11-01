466fb124e26f468b0aabacbb5127140f932470eb
chenzhiliang94 <chenzhiliang94@gmail.com>
2019-11-01 13:28:41 +0800

    Add files via upload

---
+++ b_net_A3_xx.py
@@ -0,0 +1,60 @@
+import sys
+import json
+
+class BayesianNetwork(object):
+    def __init__(self, structure, values, queries):
+        # you may add more attributes if you need
+        self.variables = structure["variables"]
+        self.dependencies = structure["dependencies"]
+        self.conditional_probabilities = values["conditional_probabilities"]
+        self.prior_probabilities = values["prior_probabilities"]
+        self.queries = queries
+        self.answer = []
+
+    def construct(self):
+        # TODO: Your code here to construct the Bayesian network
+        pass
+
+    def infer(self):
+        # TODO: Your code here to answer the queries given using the Bayesian
+        # network built in the construct() method.
+        self.answer = []  # your code to find the answer
+        # for the given example:
+        # self.answer = [{"index": 1, "answer": 0.01}, {"index": 2, "answer": 0.71}]
+        # the format of the answer returned SHOULD be as shown above.
+        return self.answer
+
+    # You may add more classes/functions if you think is useful. However, ensure
+    # all the classes/functions are in this file ONLY and used within the
+    # BayesianNetwork class.
+
+def main():
+    # STRICTLY do NOT modify the code in the main function here
+    if len(sys.argv) != 4:
+        print ("\nUsage: python b_net_A3_xx.py structure.json values.json queries.json \n")
+        raise ValueError("Wrong number of arguments!")
+
+    structure_filename = sys.argv[1]
+    values_filename = sys.argv[2]
+    queries_filename = sys.argv[3]
+
+    try:
+        with open(structure_filename, 'r') as f:
+            structure = json.load(f)
+        with open(values_filename, 'r') as f:
+            values = json.load(f)
+        with open(queries_filename, 'r') as f:
+            queries = json.load(f)
+
+    except IOError:
+        raise IOError("Input file not found or not a json file")
+
+    # testing if the code works
+    b_network = BayesianNetwork(structure, values, queries)
+    b_network.construct()
+    answers = b_network.infer()
+
+
+
+if __name__ == "__main__":
+    main()
